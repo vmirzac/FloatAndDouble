683449cf84aafccd2553ddaa1165a5076a14edf3
Victor Mirzac <Victor.Mirzac@endava.com>
2018-08-30 00:30:31 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue =5/2;
+        float myFloatValue = 5f/3;
+        double myDoubleValue = 5d/3;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("myFloatValue = "  + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+
+
+    }
+}
